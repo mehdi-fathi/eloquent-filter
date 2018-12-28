0c20ae3f768428e728c562dd61782244c64baa49
Mehdi Fathi <mehdifathi.developer@gmail.com>
2018-12-28 19:24:44 +0330

    init test

---
+++ tests/classes/BasicTest.php
@@ -0,0 +1,15 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/seeds/UserTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\classes;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::collection('users')->delete();
+        DB::collection('users')->insert(['name' => 'John Doe', 'seed' => true]);
+    }
+}
