fc181701cd45408d48cdb295d777970a14abe69c
stickler-ci <support@stickler-ci.com>
2019-07-10 11:40:06 +0000

    Fixing style errors.

--- tests/Seeds/CategoriesPostsTableSeeder.php
+++ tests/Seeds/CategoriesPostsTableSeeder.php
@@ -11,28 +11,23 @@ class CategoriesPostsTableSeeder extends Seeder
     {
         DB::table('categories_posts')->delete();
 
-        foreach (Post::all() as $index => $post):
+        foreach (Post::all() as $index => $post) :
+            foreach (range(rand(1, 4), 4) as $index_range) :
+                $category_post_id[$index]['post_id'] = $post['id'];
+                $category_post_id[$index]['created_at'] = $post['created_at'];
+                $category_post_id[$index]['updated_at'] = $post['updated_at'];
+                $category_post_id[$index]['category_id'][] = Category::inRandomOrder()->first()['id'];
+            endforeach;
 
-            foreach (range(rand(1, 4), 4) as $index_range):
-
-        $category_post_id[$index]['post_id'] = $post['id'];
-        $category_post_id[$index]['created_at'] = $post['created_at'];
-        $category_post_id[$index]['updated_at'] = $post['updated_at'];
-        $category_post_id[$index]['category_id'][] = Category::inRandomOrder()->first()['id'];
+            $category_post_id[$index]['category_id'] = array_unique($category_post_id[$index]['category_id']);
         endforeach;
 
-        $category_post_id[$index]['category_id'] = array_unique($category_post_id[$index]['category_id']);
-
-        endforeach;
-
-        foreach ($category_post_id as $category_post_id_data):
-
+        foreach ($category_post_id as $category_post_id_data) :
             $post_id = $category_post_id_data['post_id'];
-        $created_at = $category_post_id_data['created_at'];
-        $updated_at = $category_post_id_data['updated_at'];
+            $created_at = $category_post_id_data['created_at'];
+            $updated_at = $category_post_id_data['updated_at'];
 
-        foreach ($category_post_id_data['category_id'] as $category_id):
-
+            foreach ($category_post_id_data['category_id'] as $category_id) :
                 $data = [
                     'post_id'     => $post_id,
                     'category_id' => $category_id,
