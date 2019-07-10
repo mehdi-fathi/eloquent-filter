8974531adc8af9f980481cf136cd5290ed93e74d
Mehdi Fathi <mehdifathi.developer@gmail.com>
2019-07-10 05:16:42 +0000

    Apply fixes from StyleCI

--- src/queryFilter/modelFilters/Filterable.php
+++ src/queryFilter/modelFilters/Filterable.php
@@ -2,12 +2,10 @@
 
 namespace eloquentFilter\QueryFilter\modelFilters;
 
-
 use eloquentFilter\QueryFilter\queryFilter;
 
 trait Filterable
 {
-
     public function scopeFilter($query, QueryFilter $filters)
     {
         return $filters->apply($query, $this->getTable());
