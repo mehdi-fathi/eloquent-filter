bea4f6e0b63b5e4083b09a34b35ae43611c79566
Mehdi Fathi <mehdifathi.developer@gmail.com>
2019-07-10 05:54:24 +0000

    Apply fixes from StyleCI

--- tests/Seeds/CategoriesPostsTableSeeder.php
+++ tests/Seeds/CategoriesPostsTableSeeder.php
@@ -12,24 +12,24 @@ class CategoriesPostsTableSeeder extends Seeder
         foreach (\Tests\Models\Post::all() as $index => $post) :
             foreach (range(rand(1, 4), 4) as $index_range) :
                 $category_post_id[$index]['post_id'] = $post['id'];
-                $category_post_id[$index]['created_at'] = $post['created_at'];
-                $category_post_id[$index]['updated_at'] = $post['updated_at'];
+        $category_post_id[$index]['created_at'] = $post['created_at'];
+        $category_post_id[$index]['updated_at'] = $post['updated_at'];
 
-                $category_post_id[$index]['category_id'][] =
+        $category_post_id[$index]['category_id'][] =
                     \Tests\Models\Category::inRandomOrder()->first()['id'];
 
 //            $category_post_id[$index]['post_id'][] =  $post['id'];
-            endforeach;
+        endforeach;
 
-            $category_post_id[$index]['category_id'] = array_unique($category_post_id[$index]['category_id']);
+        $category_post_id[$index]['category_id'] = array_unique($category_post_id[$index]['category_id']);
         endforeach;
 
         foreach ($category_post_id as $category_post_id_data) :
             $post_id = $category_post_id_data['post_id'];
-            $created_at = $category_post_id_data['created_at'];
-            $updated_at = $category_post_id_data['updated_at'];
+        $created_at = $category_post_id_data['created_at'];
+        $updated_at = $category_post_id_data['updated_at'];
 
-            foreach ($category_post_id_data['category_id'] as $category_id) :
+        foreach ($category_post_id_data['category_id'] as $category_id) :
                 $data = [
                     'post_id'     => $post_id,
                     'category_id' => $category_id,
@@ -37,8 +37,8 @@ class CategoriesPostsTableSeeder extends Seeder
                     'updated_at'  => $updated_at,
                 ];
 
-                DB::table('categories_posts')->insert($data);
-            endforeach;
+        DB::table('categories_posts')->insert($data);
+        endforeach;
         endforeach;
 //        $faker = FakerFActory::create();
     }
