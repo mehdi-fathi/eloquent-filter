2ef591e3bb3c4a0d208ce20cf7e5625d504c2579
Mehdi Fathi <mehdifathi.developer@gmail.com>
2019-07-10 11:48:32 +0000

    Apply fixes from StyleCI

--- tests/Seeds/CategoriesPostsTableSeeder.php
+++ tests/Seeds/CategoriesPostsTableSeeder.php
@@ -2,20 +2,17 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Tests\Models\Post;
 use Tests\Models\Category;
+use Tests\Models\Post;
 
 class CategoriesPostsTableSeeder extends Seeder
 {
     public function run()
     {
-
         DB::table('categories_posts')->delete();
 
         foreach (Post::all() as $index => $post) {
-
             foreach (range(rand(1, 4), 4) as $index_range) {
-
                 $category_post_id[$index]['post_id'] = $post['id'];
                 $category_post_id[$index]['created_at'] = $post['created_at'];
                 $category_post_id[$index]['updated_at'] = $post['updated_at'];
@@ -23,22 +20,19 @@ class CategoriesPostsTableSeeder extends Seeder
             }
 
             $category_post_id[$index]['category_id'] = array_unique($category_post_id[$index]['category_id']);
-
         }
 
         foreach ($category_post_id as $category_post_id_data) {
-
             $post_id = $category_post_id_data['post_id'];
             $created_at = $category_post_id_data['created_at'];
             $updated_at = $category_post_id_data['updated_at'];
 
             foreach ($category_post_id_data['category_id'] as $category_id) {
-
                 $data = [
-                    'post_id' => $post_id,
+                    'post_id'     => $post_id,
                     'category_id' => $category_id,
-                    'created_at' => $created_at,
-                    'updated_at' => $updated_at,
+                    'created_at'  => $created_at,
+                    'updated_at'  => $updated_at,
                 ];
 
                 DB::table('categories_posts')->insert($data);
