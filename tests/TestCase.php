d475c465c4a1e8f030b5198ea865079955bc1122
Mehdi Fathi <mehdi-fathi@users.noreply.github.com>
2020-09-29 19:58:20 +0000

    Apply fixes from StyleCI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,8 +14,8 @@ class TestCase extends Orchestra\Testbench\TestCase
     {
         parent::setUp();
 
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-        $this->withFactories(__DIR__ . '/database/factories');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->withFactories(__DIR__.'/database/factories');
 
         $this->request = m::mock(\Illuminate\Http\Request::class);
 
