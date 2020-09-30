2d303272223ac04847b7e4e78232448bd5641a40
Mehdi <mehdifathi.developer@gmail.com>
2020-09-30 15:57:30 +0330

    Fix:fix test

--- tests/Tests/SeederTest.php
+++ tests/Tests/SeederTest.php
@@ -17,23 +17,21 @@ class SeederTest extends TestCase
      */
     public function testBasicSeeder()
     {
-        if (substr(app()->version(), 0, 1) != '8') {
-            if (!Schema::hasTable('users')) {
-                DB::unprepared(file_get_contents('tests/Seeds/data-sql/eloquentFilter_test.sql'));
-            }
-
-            $seeder = new UserTableSeeder();
-            $seeder->run();
-
-            $seeder = new PostTableSeeder();
-            $seeder->run();
-
-            $seeder = new CategoryTableSeeder();
-            $seeder->run();
-
-            $seeder = new CategoriesPostsTableSeeder();
-            $seeder->run();
+        if (!Schema::hasTable('users')) {
+            DB::unprepared(file_get_contents('tests/Seeds/data-sql/eloquentFilter_test.sql'));
         }
+
+        $seeder = new UserTableSeeder();
+        $seeder->run();
+
+        $seeder = new PostTableSeeder();
+        $seeder->run();
+
+        $seeder = new CategoryTableSeeder();
+        $seeder->run();
+
+        $seeder = new CategoriesPostsTableSeeder();
+        $seeder->run();
 
         $this->assertTrue(true);
         // ...
