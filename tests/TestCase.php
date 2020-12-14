a8f4a9a93b70ba0c2073839411aa2bd0f7031bdf
Mehdi <mehdifathi.developer@gmail.com>
2020-12-14 09:17:33 +0330

    Fix:clean code testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,7 @@
 <?php
 
 use eloquentFilter\QueryFilter\QueryFilter;
+use eloquentFilter\ServiceProvider;
 use Mockery as m;
 
 /**
@@ -8,6 +9,9 @@ use Mockery as m;
  */
 class TestCase extends Orchestra\Testbench\TestCase
 {
+    /**
+     * @var m\LegacyMockInterface|m\MockInterface
+     */
     public $request;
 
     public function setUp(): void
@@ -25,66 +29,5 @@ class TestCase extends Orchestra\Testbench\TestCase
                 return new QueryFilter($this->request->query());
             }
         );
-    }
-
-    /**
-     * Get application providers.
-     *
-     * @param \Illuminate\Foundation\Application $app
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            // your package service provider,
-            Orchestra\Database\ConsoleServiceProvider::class,
-            \eloquentFilter\ServiceProvider::class,
-        ];
-    }
-
-    /**
-     * Define environment setup.
-     *
-     * @param Illuminate\Foundation\Application $app
-     *
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-
-//        if (version_compare(PHP_VERSION, '7.2.0', '>=')) {
-//            // Ignores notices and reports all other kinds... and warnings
-//            error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
-//            // error_reporting(E_ALL ^ E_WARNING); // Maybe this is enough
-//        }
-//
-//        $config = require 'config/database.php';
-//
-//        $app['config']->set('app.key', 'ZsZewWyUJ5FsKp9lMwv4tYbNlegQilM7');
-//
-//        $app['config']->set('database.default', 'mysql');
-//        $app['config']->set('database.connections.mysql', $config['connections']['mysql']);
-//
-//        $app['config']->set('auth.model', 'User');
-//        $app['config']->set('auth.providers.users.model', 'User');
-//        $app['config']->set('cache.driver', 'array');
-//
-//        $app['config']->set('queue.default', 'database');
-//        $app['config']->set('queue.connections.database', [
-//            'driver' => 'mysql',
-//            'table'  => 'jobs',
-//            'queue'  => 'default',
-//            'expire' => 60,
-//        ]);
-//
-//        $host = Config::get('database.connections.mysql.host');
-//        $database = 'eloquentFilter_test';
-//        $username = Config::get('database.connections.mysql.username');
-//        $password = Config::get('database.connections.mysql.password');
-//        echo shell_exec('mysql -h '.$host.' -u '.$username.' -p'.
-//        $password.' -e "CREATE DATABASE IF NOT EXISTS '.$database.'"');
-//
-//        $app['config']->set('database.connections.mysql.database', $database);
     }
 }
